5a54fa6acd05037f0ebe4d138af01f79e0b798cc
SamvitJ <samvit.jain@gmail.com>
2016-03-01 14:48:44 -0500

    Added response handling to micropayments.js; ads removed from page if GET request to server successful.

--- micropayments/js/micropayments.js
+++ micropayments/js/micropayments.js
@@ -1,17 +1,35 @@
+var nested = false;
+
 $(document).ready(function() {
+    makeRequest();
+});
+
+function makeRequest(){
     $.ajax({
         type: "GET",
-        url: "http://10.8.235.166:5000/",
+        url: "http://10.8.235.166:5000/payable",
         crossDomain: true,
-        contentType: "application/json; charset=utf-8",
-        dataType: "plain",
+        contentType: "text/html; charset=utf-8",
         success: function(resp) {
-            console.log("Got data from server");
+            console.log("Access granted");
             console.log(resp);
+            $(".adsbygoogle").remove();
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            if (xhr.status == 402) {
+                console.log("Payment required");
+                if (!nested) {
+                    nested = true;
+                    setTimeout(function() {
+                        console.log("Making 2nd request...");
+                        makeRequest();
+                    }, 2000);
+                }
+            }
         },
         failure: function(err) {
             console.log("Failed to get data");
             alert(err);
         }
     })
-});
+}
